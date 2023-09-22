e87f1f8188124bed13ba986b279e9c3d9029c405
DouTunLH <dtnbdlkm@gmail.com>
2023-09-22 10:38:17 +0700

    full screen detail view

--- app/src/main/java/com/example/necomovie/DetailActivity.java
+++ app/src/main/java/com/example/necomovie/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.necomovie;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
@@ -9,12 +10,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -28,16 +34,24 @@ import com.example.necomovie.model.Trailer;
 import com.example.necomovie.ui.favourite.FavouriteFragment;
 import com.example.necomovie.ui.favourite.FavouriteViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 import java.util.List;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
 
 
 public class DetailActivity extends AppCompatActivity {
+    private Boolean isFullscreen = false;
+    YouTubePlayer youTubePlayer;
     YouTubePlayerView youTubePlayerView;
+    FrameLayout fullscreenViewContainer;
     TextView titleTextView;
     TextView yearTextView;
     TextView descriptionTextView;
@@ -46,12 +60,25 @@ public class DetailActivity extends AppCompatActivity {
     MaterialToolbar toolbar;
     ImageButton backButton;
     Button favouriteBtn;
+    private final OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+        @Override
+        public void handleOnBackPressed() {
+            if (isFullscreen) {
+                youTubePlayer.toggleFullscreen();
+            } else {
+                finish();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getOnBackPressedDispatcher().addCallback(onBackPressedCallback);
         setContentView(R.layout.activity_detail);
         detailViewModel = new ViewModelProvider(this).get(DetailViewModel.class);
         detailViewModel.movie = getIntent().getParcelableExtra("movie");
+        fullscreenViewContainer = findViewById(R.id.full_screen_view_container);
         youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
         toolbar = (MaterialToolbar) findViewById(R.id.detailToolbar);
@@ -72,12 +99,52 @@ public class DetailActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new GridSpacingItemDecoration(3, 20));
         recyclerView.setAdapter(adapter);
         detailViewModel.fetchData();
+        View decorView = getWindow().getDecorView();
+        IFramePlayerOptions iFramePlayerOptions = new IFramePlayerOptions.Builder()
+                .controls(1)
+                .fullscreen(1)
+                .build();
+        youTubePlayerView.addFullscreenListener(new FullscreenListener() {
 
+            @Override
+            public void onEnterFullscreen(@NonNull View view, @NonNull Function0<Unit> function0) {
+                isFullscreen = true;
+                youTubePlayerView.setVisibility(View.GONE);
+                fullscreenViewContainer.setVisibility(View.VISIBLE);
+                fullscreenViewContainer.addView(view);
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+            @Override
+            public void onExitFullscreen() {
+                isFullscreen = false;
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                youTubePlayerView.setVisibility(View.VISIBLE);
+                fullscreenViewContainer.setVisibility(View.GONE);
+                fullscreenViewContainer.removeAllViews();
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            }
+        });
+        youTubePlayerView.initialize(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                DetailActivity.this.youTubePlayer = youTubePlayer;
+            }
+
+            @Override
+            public void onStateChange(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerState state) {
+                super.onStateChange(youTubePlayer, state);
+                if(state == PlayerConstants.PlayerState.PLAYING && isFullscreen){
+                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+                }
+            }
+        }, iFramePlayerOptions);
         favouriteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 detailViewModel.setFavourite();
-
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -90,7 +157,7 @@ public class DetailActivity extends AppCompatActivity {
         detailViewModel.isFavourite.observe(this, new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
-                if(aBoolean){
+                if (aBoolean) {
                     favouriteBtn.setText("Remove from favourite");
                 } else {
                     favouriteBtn.setText("Add to favourite");
@@ -103,6 +170,7 @@ public class DetailActivity extends AppCompatActivity {
                 youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
                     @Override
                     public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+
                         for (Trailer trailer : detailViewModel.trailers.getValue()) {
                             if ("Trailer".equals(trailer.type)) {
                                 youTubePlayer.cueVideo(trailer.key, 0);
@@ -123,4 +191,11 @@ public class DetailActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE && isFullscreen) {
+
+        }
+    }
 }
